71099e9ee29c5f4d0bbf6289c83a96d6d809f90f
Max McGregor <editing4life@gmail.com>
2017-12-10 17:09:22 +1300

    10-12-17 Backup

---
+++ Maps/Tile Sets/41.tsx
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="terrain" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../../Assets/Troll Bridge Studios/2D Starter Kit/Art/EnvironmentTiles/terrain.png" width="1024" height="1024"/>
+ <terraintypes>
+  <terrain name="Dirt1" tile="97"/>
+  <terrain name="Dirt 2" tile="97"/>
+  <terrain name="Dirt 3" tile="97"/>
+  <terrain name="Grass" tile="97"/>
+  <terrain name="New Terrain" tile="289"/>
+  <terrain name="Grass" tile="289"/>
+  <terrain name="Waters Edge" tile="124"/>
+  <terrain name="Beach Edge" tile="310"/>
+ </terraintypes>
+ <tile id="1" terrain="0,0,0,"/>
+ <tile id="2" terrain="0,0,,0"/>
+ <tile id="4" terrain="1,1,1,"/>
+ <tile id="5" terrain="1,1,,1"/>
+ <tile id="7" terrain="2,2,2,"/>
+ <tile id="8" terrain="2,2,,2"/>
+ <tile id="28" terrain="6,6,6,"/>
+ <tile id="29" terrain="6,6,,6"/>
+ <tile id="33" terrain="0,,0,0"/>
+ <tile id="34" terrain=",0,0,0"/>
+ <tile id="36" terrain="1,,1,1"/>
+ <tile id="37" terrain=",1,1,1"/>
+ <tile id="39" terrain="2,,2,2"/>
+ <tile id="40" terrain=",2,2,2"/>
+ <tile id="60" terrain="6,,6,6"/>
+ <tile id="61" terrain=",6,6,6"/>
+ <tile id="64" terrain=",,,0"/>
+ <tile id="65" terrain=",,0,0"/>
+ <tile id="66" terrain=",,0,"/>
+ <tile id="67" terrain=",,,1"/>
+ <tile id="68" terrain=",,1,1"/>
+ <tile id="69" terrain=",,1,"/>
+ <tile id="70" terrain=",,,2"/>
+ <tile id="71" terrain=",,2,2"/>
+ <tile id="72" terrain=",,2,"/>
+ <tile id="91" terrain=",,,6"/>
+ <tile id="92" terrain=",,6,6"/>
+ <tile id="93" terrain=",,6,"/>
+ <tile id="96" terrain=",0,,0"/>
+ <tile id="98" terrain="0,,0,"/>
+ <tile id="99" terrain=",1,,1"/>
+ <tile id="100" terrain="1,1,1,1"/>
+ <tile id="101" terrain="1,,1,"/>
+ <tile id="102" terrain=",2,,2"/>
+ <tile id="103" terrain="2,2,2,2"/>
+ <tile id="104" terrain="2,,2,"/>
+ <tile id="123" terrain=",6,,6"/>
+ <tile id="124" terrain="6,6,6,6"/>
+ <tile id="125" terrain="6,,6,"/>
+ <tile id="128" terrain=",0,,"/>
+ <tile id="129" terrain="0,0,,"/>
+ <tile id="130" terrain="0,,,"/>
+ <tile id="131" terrain=",1,,"/>
+ <tile id="132" terrain="1,1,,"/>
+ <tile id="133" terrain="1,,,"/>
+ <tile id="134" terrain=",2,,"/>
+ <tile id="135" terrain="2,2,,"/>
+ <tile id="136" terrain="2,,,"/>
+ <tile id="155" terrain=",6,,"/>
+ <tile id="156" terrain="6,6,,"/>
+ <tile id="157" terrain="6,,,"/>
+ <tile id="193" terrain="5,5,5,"/>
+ <tile id="194" terrain="5,5,,5"/>
+ <tile id="214" terrain="7,7,7,"/>
+ <tile id="215" terrain="7,7,,7"/>
+ <tile id="225" terrain="5,,5,5"/>
+ <tile id="226" terrain=",5,5,5"/>
+ <tile id="246" terrain="7,,7,7"/>
+ <tile id="247" terrain=",7,7,7"/>
+ <tile id="256" terrain=",,,5"/>
+ <tile id="257" terrain=",,5,5"/>
+ <tile id="258" terrain=",,5,"/>
+ <tile id="277" terrain=",,,7"/>
+ <tile id="278" terrain=",,7,7"/>
+ <tile id="279" terrain=",,7,"/>
+ <tile id="288" terrain=",5,,5"/>
+ <tile id="289" terrain="5,5,5,5"/>
+ <tile id="290" terrain="5,,5,"/>
+ <tile id="309" terrain=",7,,7"/>
+ <tile id="310" terrain="7,7,7,7"/>
+ <tile id="311" terrain="7,,7,"/>
+ <tile id="320" terrain=",5,,"/>
+ <tile id="321" terrain="5,5,,"/>
+ <tile id="322" terrain="5,,,"/>
+ <tile id="341" terrain=",7,,"/>
+ <tile id="342" terrain="7,7,,"/>
+ <tile id="343" terrain="7,,,"/>
+</tileset>
---
+++ Maps/Tile Sets/43.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="plant repack v2" tilewidth="32" tileheight="128" tilecount="39" columns="13">
+ <image source="../../Assets/Troll Bridge Studios/2D Starter Kit/Art/EnvironmentTiles/plant repack.png" width="416" height="416"/>
+</tileset>
