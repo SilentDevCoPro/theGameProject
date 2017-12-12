f0d10086ca0fb7686886cc74627b0be72c32abde
Max McGregor <editing4life@gmail.com>
2017-12-12 13:03:25 +1300

    Castle Map Updated

--- Maps/Tile Sets/42.tsx
+++ Maps/Tile Sets/42.tsx
@@ -1,4 +1,100 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="PathAndObjects" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="../../Assets/Troll Bridge Studios/2D Starter Kit/Art/EnvironmentTiles/PathAndObjects.png" width="512" height="512"/>
+ <terraintypes>
+  <terrain name="Cobble Grass" tile="23"/>
+  <terrain name="Dirt Grass" tile="26"/>
+  <terrain name="White Cobble Grass" tile="20"/>
+  <terrain name="Grey Cobble Grass" tile="17"/>
+  <terrain name="#2 White Cobble Grass" tile="100"/>
+  <terrain name="#2 Dirt Grass" tile="103"/>
+  <terrain name="#3 Dirt Grass" tile="106"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,3"/>
+ <tile id="1" terrain=",,3,3"/>
+ <tile id="2" terrain=",,3,"/>
+ <tile id="3" terrain=",,,2"/>
+ <tile id="4" terrain=",,2,2"/>
+ <tile id="5" terrain=",,2,"/>
+ <tile id="6" terrain=",,,0"/>
+ <tile id="7" terrain=",,0,0"/>
+ <tile id="8" terrain=",,0,"/>
+ <tile id="9" terrain=",,,1"/>
+ <tile id="10" terrain=",,1,1"/>
+ <tile id="11" terrain=",,1,"/>
+ <tile id="16" terrain=",3,,3"/>
+ <tile id="17" terrain="3,3,3,3"/>
+ <tile id="18" terrain="3,,3,"/>
+ <tile id="19" terrain=",2,,2"/>
+ <tile id="20" terrain="2,2,2,2"/>
+ <tile id="21" terrain="2,,2,"/>
+ <tile id="22" terrain=",0,,0"/>
+ <tile id="23" terrain="0,0,0,0"/>
+ <tile id="24" terrain="0,,0,"/>
+ <tile id="25" terrain=",1,,1"/>
+ <tile id="26" terrain="1,1,1,1"/>
+ <tile id="27" terrain="1,,1,"/>
+ <tile id="32" terrain=",3,,"/>
+ <tile id="33" terrain="3,3,,"/>
+ <tile id="34" terrain="3,,,"/>
+ <tile id="35" terrain=",2,,"/>
+ <tile id="36" terrain="2,2,,"/>
+ <tile id="37" terrain="2,,,"/>
+ <tile id="38" terrain=",0,,"/>
+ <tile id="39" terrain="0,0,,"/>
+ <tile id="40" terrain="0,,,"/>
+ <tile id="41" terrain=",1,,"/>
+ <tile id="42" terrain="1,1,,"/>
+ <tile id="43" terrain="1,,,"/>
+ <tile id="48" terrain="3,3,3,"/>
+ <tile id="49" terrain="3,3,,3"/>
+ <tile id="51" terrain="2,2,2,"/>
+ <tile id="52" terrain="2,2,,2"/>
+ <tile id="54" terrain="0,0,0,"/>
+ <tile id="55" terrain="0,0,,0"/>
+ <tile id="57" terrain="1,1,1,"/>
+ <tile id="58" terrain="1,1,,1"/>
+ <tile id="64" terrain="3,,3,3"/>
+ <tile id="65" terrain=",3,3,3"/>
+ <tile id="67" terrain="2,,2,2"/>
+ <tile id="68" terrain=",2,2,2"/>
+ <tile id="70" terrain="0,,0,0"/>
+ <tile id="71" terrain=",0,0,0"/>
+ <tile id="73" terrain="1,,1,1"/>
+ <tile id="74" terrain=",1,1,1"/>
+ <tile id="83" terrain=",,,4"/>
+ <tile id="84" terrain=",,4,4"/>
+ <tile id="85" terrain=",,4,"/>
+ <tile id="86" terrain=",,,5"/>
+ <tile id="87" terrain=",,5,5"/>
+ <tile id="88" terrain=",,5,"/>
+ <tile id="89" terrain=",,,6"/>
+ <tile id="90" terrain=",,6,6"/>
+ <tile id="91" terrain=",,6,"/>
+ <tile id="99" terrain=",4,,4"/>
+ <tile id="100" terrain="4,4,4,4"/>
+ <tile id="101" terrain="4,,4,"/>
+ <tile id="102" terrain=",5,,5"/>
+ <tile id="103" terrain="5,5,5,5"/>
+ <tile id="104" terrain="5,,5,"/>
+ <tile id="105" terrain=",6,,6"/>
+ <tile id="106" terrain="6,6,6,6"/>
+ <tile id="107" terrain="6,,6,"/>
+ <tile id="115" terrain=",4,,"/>
+ <tile id="116" terrain="4,4,,"/>
+ <tile id="117" terrain="4,,,"/>
+ <tile id="118" terrain=",5,,"/>
+ <tile id="119" terrain="5,5,,"/>
+ <tile id="120" terrain="5,,,"/>
+ <tile id="121" terrain=",6,,"/>
+ <tile id="122" terrain="6,6,,"/>
+ <tile id="123" terrain="6,,,"/>
+ <tile id="131" terrain="4,4,4,"/>
+ <tile id="132" terrain="4,4,,4"/>
+ <tile id="134" terrain="5,5,5,"/>
+ <tile id="135" terrain="5,5,,5"/>
+ <tile id="147" terrain="4,,4,4"/>
+ <tile id="148" terrain=",4,4,4"/>
+ <tile id="150" terrain="5,,5,5"/>
+ <tile id="151" terrain=",5,5,5"/>
 </tileset>
