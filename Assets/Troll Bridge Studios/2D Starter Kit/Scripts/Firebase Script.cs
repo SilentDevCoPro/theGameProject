0b0fba79f208af7ea2500ef8cbaeb424595a9056
Max McGregor <editing4life@gmail.com>
2017-12-29 19:47:40 +1300

    Updated GUI

---
+++ Assets/Troll Bridge Studios/2D Starter Kit/Scripts/Firebase Script.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class NewBehaviourScript : MonoBehaviour {
+    public InputField EmailAddress, Password;
+    public string loginScene, newUserScene;
+
+    public void LoginButtonPressed() {
+        FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(EmailAddress.text, Password.text).
+        ContinueWith((obj) =>
+        {
+            SceneManager.LoadScene(loginScene);
+        });
+    }
+
+    public void NewUseButtonPressed() {
+        FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(EmailAddress.text, Password.text).
+        ContinueWith((obj) =>
+        {
+            SceneManager.LoadScene(newUserScene);
+        });
+    }
+}
