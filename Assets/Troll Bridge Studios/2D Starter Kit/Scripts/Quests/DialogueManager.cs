2be08a7e3ef4369b242f410b0e130e637df636ed
Max McGregor <editing4life@gmail.com>
2018-01-10 14:38:35 +1300

    Quest System Update - Start quest activates objects

--- Assets/Troll Bridge Studios/2D Starter Kit/Scripts/Quests/DialogueManager.cs
+++ Assets/Troll Bridge Studios/2D Starter Kit/Scripts/Quests/DialogueManager.cs
@@ -8,28 +8,31 @@ namespace TrollBridge
     {
 
         Action_Key_Dialogue action_Key_dialogue;
-        public GameObject dialogueToDisable;
-        public GameObject[] gameObjectToEnable;
+        public GameObject questDialogue;
+        public GameObject[] dialoguesToChangeState;
 
         // Use this for initialization
         void Start()
         {
-            if(dialogueToDisable != null)
-                action_Key_dialogue = dialogueToDisable.GetComponent<Action_Key_Dialogue>();
+            if(questDialogue != null)
+                action_Key_dialogue = questDialogue.GetComponent<Action_Key_Dialogue>();
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (dialogueToDisable != null)
+            if (questDialogue != null)
             {
                 if (action_Key_dialogue.completed == true)
                 {
-                    foreach (GameObject obj in gameObjectToEnable)
+                    foreach (GameObject obj in dialoguesToChangeState)
                     {
-                        obj.SetActive(true);
+                        if (!obj.activeSelf)
+                            obj.SetActive(true);
+                        else
+                            obj.SetActive(false);
                     }
-                    dialogueToDisable.SetActive(false);
+                    DestroyObject(this);
                 }
             }
         }
---
+++ Assets/Troll Bridge Studios/2D Starter Kit/Scripts/Quests/OnQuestStart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnQuestStart : MonoBehaviour {
+
+    public int questID;
+    public GameObject[] gameObjectsToChangeState;
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < QuestManager.questManager.currentQuestList.Count; i++)
+        {
+            if(QuestManager.questManager.currentQuestList[i].id == questID)
+            {
+                if(QuestManager.questManager.currentQuestList[i].progress == Quest.QuestProgress.ACCEPTED)
+                {
+                    foreach(GameObject obj in gameObjectsToChangeState)
+                    {
+                        if (!obj.activeSelf)
+                            obj.SetActive(true);
+                        else
+                            obj.SetActive(false);
+                    }
+                    Destroy(this);
+                }
+            }
+        }
+	}
+}
