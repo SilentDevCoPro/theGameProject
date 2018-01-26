c616a13b492937a0edd9f67ae6eb114cba4d2616
Max McGregor <editing4life@gmail.com>
2018-01-26 17:42:24 +1300

    Quest Log Written

---
+++ Assets/Troll Bridge Studios/2D Starter Kit/Scripts/ChangeObjectState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeObjectState : MonoBehaviour {
+
+    public GameObject goToChangeState;
+
+    public void ChangeState()
+    {
+        goToChangeState.SetActive(false);
+    }
+}
