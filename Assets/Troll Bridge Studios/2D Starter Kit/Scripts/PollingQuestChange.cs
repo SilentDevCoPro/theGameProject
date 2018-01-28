97ca0eb67a586063a7a5eafcaf4240e9533ffd27
Max McGregor <editing4life@gmail.com>
2018-01-28 17:28:53 +1300

    Quest Dialogue Updated

---
+++ Assets/Troll Bridge Studios/2D Starter Kit/Scripts/PollingQuestChange.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PollingQuestChange: MonoBehaviour {
+
+    public int questID;
+    public GameObject[] gameObjectsToActivate;
+    public GameObject[] gameObjectsToDeactivate;
+
+    // Update is called once per frame
+    void Update () {
+        for (int i = 0; i < QuestManager.questManager.questList.Count; i++)
+        {
+            if (QuestManager.questManager.questList[i].id == questID)
+            {
+                if (QuestManager.questManager.questList[i].progress == Quest.QuestProgress.ACCEPTED ||
+                    QuestManager.questManager.questList[i].progress == Quest.QuestProgress.DONE)
+                {
+                    foreach (GameObject obj in gameObjectsToActivate)
+                    {
+                        if (!obj.activeSelf)
+                        {
+                            Debug.Log("George IS NOW ACTIVE");
+                            obj.SetActive(true);
+                        }
+                    }
+
+                    foreach (GameObject obj in gameObjectsToDeactivate)
+                    {
+                        if (obj.activeSelf)
+                            obj.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+}
+
