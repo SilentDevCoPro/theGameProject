b3f2e666ff4424ddc982e3c9c55f0a6f3e7fc2c9
Max McGregor <editing4life@gmail.com>
2018-02-01 20:59:51 +1300

    Boss NPC Updated

---
+++ Assets/Troll Bridge Studios/2D Starter Kit/Scripts/Than Fight/Laser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    private LineRenderer lineRenderer;
+    public Transform laserHit;
+
+
+	// Use this for initialization
+	void Start () {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.useWorldSpace = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, laserHit.transform.position);
+        lineRenderer.enabled = true;
+	}
+}
